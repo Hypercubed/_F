cc88d67567e9333993af718ac0d3cd1ae4214081
J. Harshbarger <hypercubed@gmail.com>
2014-12-21 21:13:09 +0900

    WIP

--- _F.js
+++ _F.js
@@ -9,24 +9,112 @@
 ;(function() {
   'use strict';
 
-  // (private) For each key in an object
-  function each(object, fn) {
-    Object.keys(object).forEach(function(key) {
+  var R;
+
+  try {
+    R = require('ramda');
+  } catch(err) {
+
+  }
+
+  if (false) {
+
+    R = {};
+
+    R.keys = Object.keys;
+
+    R.identity = function identity(d) {
+      return d;
+    };
+
+    R.lPartial = function partial(func) {
+      var boundArgs = Array.prototype.slice.call(arguments, 1);
+      return function() {
+        var position = 0;
+        var args = boundArgs.slice();
+        while (position < arguments.length) args.push(arguments[position++]);
+        return func.apply(this, args);
+      };
+    };
+
+    R.prop = function prop(p, obj) {
+      if (arguments.length === 1) {
+        return function _prop(obj) { return obj[p]; };
+      }
+      return obj[p];
+    };
+
+    R.type = function type(val) {
+      return val === null      ? 'Null'      :
+             val === undefined ? 'Undefined' :
+             toString.call(val).slice(8, -1);
+    };
+
+    R.compose = function _compose(g, f) {
+      return function() {
+        return g.call(this, f.apply(this, arguments));
+      };
+    };
+
+    R.and = function and(f, g) {
+      return function _and() {
+        return f.apply(this, arguments) && g.apply(this, arguments);
+      };
+    };
+
+    R.or = function(f, g) {
+      return function _or() {
+        return f.apply(this,arguments) || g.apply(this,arguments);
+      };
+    };
+
+    R.not = function(g) {
+      return function _not() {
+        return !g.apply(this,arguments);
+      };
+    };
+
+    R.arity = function(n, fn) {
+      //switch (n) {
+        //case 0: return function() {return fn.apply(this, arguments);};
+        //case 1:
+          return function(a0) {void a0; return fn.apply(this, arguments);};
+        //case 2: return function(a0, a1) {void a1; return fn.apply(this, arguments);};
+        //case 3: return function(a0, a1, a2) {void a2; return fn.apply(this, arguments);};
+        //case 4: return function(a0, a1, a2, a3) {void a3; return fn.apply(this, arguments);};
+        //case 5: return function(a0, a1, a2, a3, a4) {void a4; return fn.apply(this, arguments);};
+        //case 6: return function(a0, a1, a2, a3, a4, a5) {void a5; return fn.apply(this, arguments);};
+        //case 7: return function(a0, a1, a2, a3, a4, a5, a6) {void a6; return fn.apply(this, arguments);};
+        //case 8: return function(a0, a1, a2, a3, a4, a5, a6, a7) {void a7; return fn.apply(this, arguments);};
+        //case 9: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8) {void a8; return fn.apply(this, arguments);};
+        //case 10: return function(a0, a1, a2, a3, a4, a5, a6, a7, a8, a9) {void a9; return fn.apply(this, arguments);};
+        //default: return fn; // TODO: or throw?
+      //}
+    };
+
+    R.bind = function bind(fn, thisObj) {
+      return function() {
+        return fn.apply(thisObj, arguments);
+      };
+    };
+
+  }
+
+  // (private)
+  function _each(object, fn) {
+    R.keys(object).forEach(function(key) {
       fn(object[key], key);
     });
   }
 
   // (private) Copies from one object to another
-  function extend(dest, src) {
-    each(src, function(value, key) {
-      dest[key] = value;
-    });
-    return dest;
-  }
-
-  // (private)
-  function identity(d) {
-    return d;
+  function _extend(destination, other) {
+    var props = R.keys(other),
+    idx = -1, length = props.length;
+    while (++idx < length) {
+      destination[props[idx]] = other[props[idx]];
+    }
+    return destination;
   }
 
   // (private)
@@ -35,49 +123,13 @@
   }
 
   // (private)
-  function ascending(a, b) {
+  function ascending(a, b) {  // R.comparator(R.lt)
     return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
   }
 
   // (private)
-  function descending(a, b) {
+  function descending(a, b) {  // R.comparator(R.gt)
     return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
-  }
-
-  // (private)
-  function apply(f) {
-    return function() { return f.apply(this, arguments); };
-  }
-
-  // (private)
-  function partial(func) {
-    var boundArgs = Array.prototype.slice.call(arguments, 1);
-    return function() {
-      var position = 0;
-      var args = boundArgs.slice();
-      while (position < arguments.length) args.push(arguments[position++]);
-      return func.apply(this, args);
-    };
-  }
-
-  // (private) constructs a accessor function based on a key
-  function prop(key) {
-    if (!exists(key)) return identity;
-    if (typeof key === 'function') return apply(key);
-    if (key === '$index') return function(d, i) { return i; };
-    if (key === '$this') return function() { return this; };
-    //if (typeof key === 'string' && key.match(/^\$\d+$/)) { key = parseInt(key.replace('$')); };
-    if (typeof key === 'number') {
-      return function(d) { return exists(d) ? d[key] : undefined; };
-    }
-    if (typeof key === 'string' && key.indexOf('.') === -1 ) {
-      return function(d) { return exists(d) ? d[key] : undefined; };
-    }
-
-    var chain = (Array.isArray(key)) ? key : key.split('.');
-    var f = prop(chain.shift());
-    var g = prop(chain.join('.'));
-    return compose ( g, f );
   }
 
   // (private)
@@ -90,20 +142,54 @@
   // (private)
   function curry2(fn) {
     return function() {
-      var vArgs = Array.prototype.slice.call(arguments, 0);
+      var vArgs = Array.prototype.slice.call(arguments);
       return function(a) {
         return fn.apply(this, [a].concat(vArgs));
       };
     };
   }
 
-  // (private)
-  function curry5(fn) {
-    return function(f,g) {
-      return function(a, i, d) {
-        return !!fn.call(this,f, g, a, i, d);
-      };
+  function _flip(fn) {
+    return function _flipped() {
+      var args =　Array.prototype.slice.call(arguments);
+      return fn.apply(this, args.reverse());
     };
+  }
+
+  // (private)
+  function flip(fn) {
+    return function() {
+      var _fn = fn.apply(this,arguments);
+      return _flip(_fn);
+    };
+  }
+
+  var _prop = function prop(p, obj) {
+    if (arguments.length === 1) {
+      return function _prop(d) { return exists(d) ? d[p] : undefined; };
+    }
+    return obj[p];
+  };
+
+
+  // (private) constructs a accessor function based on a key
+  function prop(key) {
+    if (!exists(key)) return R.identity;
+    if (typeof key === 'function') return R.arity(1,key);
+    if (key === '$index') return function(d, i) { return i; };
+    if (key === '$this') return function() { return this; };
+    //if (typeof key === 'string' && key.match(/^\$\d+$/)) { key = parseInt(key.replace('$')); };
+    if (typeof key === 'number') {
+      return _prop(key);
+    }
+    if (typeof key === 'string' && key.indexOf('.') === -1 ) {
+      return _prop(key);
+    }
+
+    var chain = (Array.isArray(key)) ? key : key.split('.');
+    var f = prop(chain.shift());
+    var g = prop(chain.join('.'));
+    return R.compose(g,f);
   }
 
   // Prototype of _F functions
@@ -114,15 +200,15 @@
   // Operators take a value and return a new accessor function
   var _proto_ops = {
     eq:       function(a,v)   { return a  == v; },
-    is:       function(a,v)   { return a === v; },
-    neq:      function(a,v)   { return a !== v; },
-    lt:       function(a,v)   { return a  <  v; },
-    gt:       function(a,v)   { return a  >  v; },
-    lte:      function(a,v)   { return a  <= v; },
-    gte:      function(a,v)   { return a  >= v; },
+    is:       R.eq  || function(a,v)   { return a === v; },
+    neq:      R.compose(R.not, R.eq) || function(a,v)   { return a !== v; },
+    lt:       R.lt  || function(a,v)   { return a  <  v; },
+    gt:       R.gt  || function(a,v)   { return a  >  v; },
+    lte:      R.lte || function(a,v)   { return a  <= v; },
+    gte:      R.gte || function(a,v)   { return a  >= v; },
     between:  function(a,v,w) { return a > v && a < w; },
-    exists:   function(a  )   { return a !== undefined && a !== null; },
-    typeof:   function(a,v)   { return typeof a === v; },
+    exists:   exists,
+    typeof:   function(a,v)   { return R.type(a).toLowerCase() === v; },
     match:    function(a,v)   { return (v instanceof RegExp) ? v.test(String(a)) : (new RegExp(v)).test(String(a)); },
     in:       function(a,v)   { return (Array.isArray(v)) ? v.indexOf(a) > -1 : String(v).indexOf(String(a)) > -1; },
     has:      function(a,v)   { return (Array.isArray(a)) ? a.indexOf(v) > -1 : String(a).indexOf(String(v)) > -1; }
@@ -133,14 +219,10 @@
   // Chaining functions take a two accessor functions and return a new accessor, returning `true` or `false`
   // The second function will not be called if the first returns false
   var _proto_chains = {
-    and: function(f, g, a, i, d) {
-      return f.call(this,d,i) && g.call(this,d,i);
-    },
-    or:  function(f, g, a, i, d) {
-      return f.call(this, d, i) || g.call(this, d, i);
-    },
-    not: function(f, g, a, i, d) {
-      return !g.call(this, d, i);
+    and: R.and,
+    or: R.or,
+    not: function(f, g) {
+      return R.not(g);
     }
   };
 
@@ -167,28 +249,27 @@
     return g;
   };
 
-  _proto.compose = partial(compose, _proto.factory);
-
   _proto.partial = function() {
-    var _fn = partial.apply(this,arguments);
-    return partial(_fn, this);
+    var args = Array.prototype.slice.call(arguments);
+    args.push(this);
+    return R.lPartial.apply(this,args);
   };
 
   _proto.wrap = function(f) {
-    var _g = compose(this.partial.apply(this,arguments), this._factory);
-    return this.compose(_g);
+    var _g = R.compose(this.partial.apply(this,arguments), this._factory);
+    return R.compose(_proto.factory, _g);
   };
 
   _proto.chain = function(fn) {
     return function() {
       var self = this;
-      var _fn = this.wrap(fn);
+      var _fn = this.wrap(flip(fn));
 
       if (arguments.length < 1) {
         var f = {};
 
-        each(_proto_ops, function(op,k) {
-          f[k] = compose(_fn, self[k]).bind(self);
+        _each(_proto_ops, function(op,k) {
+          f[k] = R.bind(R.compose(_fn, self[k]),self);
         });
 
         return f;
@@ -198,7 +279,7 @@
     };
   };
 
-  _proto.order = function(comparator) {
+  _proto.order = function(pred) {
     var self = this;
 
     if (arguments.length < 1) {
@@ -209,19 +290,19 @@
     }
 
     return function(a,b) {
-      return comparator(self(a),self(b));
+      return pred(self(a),self(b));
     };
 
   };
 
   // Wraps operators in factor generator
-  each(_proto_ops, function(v,k) {
-    _proto[k] = compose(_proto.factory, curry2(v));
+  _each(_proto_ops, function(v,k) {
+    _proto[k] = R.compose(_proto.factory, curry2(v));
   });
 
   // Wraps chain functions (and, or, not) and adds to prototype
-  each(_proto_chains, function(v,o) {
-    _proto[o] = _proto.chain(curry5(v));
+  _each(_proto_chains, function(v,o) {
+    _proto[o] = _proto.chain(v);
   });
 
   // _F factory
@@ -235,11 +316,10 @@
     // Create base function object
     _fn = (typeof ret === 'function') ? compose(ret, _accessor) : _accessor;
     _fn.key = key;
-    _fn.accessor = (key !== undefined && key !== null) ? _accessor : undefined;
+    _fn.accessor = exists(key) ? _accessor : undefined;
 
-    extend(_fn, _proto);
+    return _extend(_fn, _proto);
 
-    return _fn;
   }
 
   // wrap up for Node.js or the browser
@@ -256,29 +336,3 @@
 }).call(function() {
   return this || (typeof window !== 'undefined' ? window : global);
 }());
-
-// Function.prototype.bind shim
-// -----------
-// See [phantomjs/issues/10522](https://github.com/ariya/phantomjs/issues/10522).
-;(function() {
-  if (!Function.prototype.bind) {
-    Function.prototype.bind = function (oThis) {
-      if (typeof this !== "function") {
-        throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
-      }
-
-      var aArgs = Array.prototype.slice.call(arguments, 1),
-          fToBind = this,
-          fNOP = function () {},
-          fBound = function () {
-            return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
-                                 aArgs.concat(Array.prototype.slice.call(arguments)));
-          };
-
-      fNOP.prototype = this.prototype;
-      fBound.prototype = new fNOP();
-
-      return fBound;
-    };
-  }
-})();
